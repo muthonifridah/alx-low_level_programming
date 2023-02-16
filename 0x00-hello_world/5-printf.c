b7ef3c8022828987108307ae57082ee2450e73ea
muthonifridah <gitarifridah2016@gmail.com>
2023-02-16 11:58:53 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - a program that print a line using printf finction
+ * Return: 0 (success)
+ */
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+	return (0);
+}
+
