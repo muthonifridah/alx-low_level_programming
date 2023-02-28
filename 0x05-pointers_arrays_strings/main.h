9a90aedc03d5bdcf9585217eeb9651da475f9614
muthonifridah <gitarifridah2016@gmail.com>
2023-02-28 09:42:19 -0800

    ./9-strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void rev_string(char *s);
 void pits2(char *str);
 void put_half(char *str);
 void print_array(int*a, int n);
-char _strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
