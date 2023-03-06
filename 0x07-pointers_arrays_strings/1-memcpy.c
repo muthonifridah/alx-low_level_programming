17e1b9b239ce64494960e6d1cef0dc8cdda1af6f
muthonifridah <gitarifridah2016@gmail.com>
2023-03-06 13:46:04 -0800

    ./1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point.
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
